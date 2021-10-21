efda5aa7bc57d47a4f933d61cd80e8c878c54b99
DeepDarkChocolate <yhkwon@iastate.edu>
2021-10-20 23:18:45 -0500

    update

--- Bayesian/Canadian/ftns.jl
+++ Bayesian/Canadian/ftns.jl
@@ -14,19 +14,63 @@ logπ4 = function(eta; x_sampled = x_sampled::Vector{Float64},
     n = size(x_sampled)[1]
     K = size(nodes)[1]
 
-    return ((sigma2 > 0.0) & all(phi .> 0.0) & all(0.0 .< mu .< 1.0)) ? (
+    #return ((sigma2 > 0.0) & all(phi .> 0.0) & all(0.0 .< mu .< 1.0)) ? (
+    return ((sigma2 > 0.0) & all(phi .> 0.0) & all((1 / 10000) < mu[1] < (1 / 1000) <  mu[2] < (1 / 300) < mu[3]  < 1.0)) ? (
     sigma = sqrt(sigma2);
     PI = (exp.(hcat(ones(n), x_sampled, y_sampled) * alpha) * mu)./
+    (exp.(hcat(ones(n), x_sampled, y_sampled) * alpha) * ones(3));
+    #@show PI;
+
+    #PI_tmp = [sum([mu[h] * exp(alpha[1, h] + alpha[2, h] * x_sampled[i] + alpha[3, h] * y_sampled[i]) for h in 1:3]) / sum([exp(alpha[1, h] + alpha[2, h] * x_sampled[i] + alpha[3, h] * y_sampled[i]) for h in 1:3]) for i in 1:n];
+    #@show all(PI .== PI_tmp);
+
+    denom = [sum((exp.(hcat(ones(K), fill(x_sampled[i], K),
+    (@. nodes * sigma + theta0 + theta1 * x_sampled[i])) * alpha) * mu) ./
+    (exp.(hcat(ones(K), fill(x_sampled[i], K),
+    (@. nodes * sigma + theta0 + theta1 * x_sampled[i])) * alpha) * ones(3)) .* weights)
+    for i in 1:n];
+
+    fw = [StatsBase.mean((@. pdf(BetaPrime((1 - mu) * phi, mu * phi + 1), w_sampled[i] - 1.0)),
+    StatsBase.weights(vcat(exp.(hcat(1.0, x_sampled[i], y_sampled[i]) * alpha)...) .* mu))
+    for i in 1:n];
+    #fw_tmp = [sum([pdf(BetaPrime((1 - mu[h]) * phi[h], mu[h] * phi[h] + 1), w_sampled[i] - 1.0) * mu[h] * exp(alpha[1, h] + alpha[2, h] * x_sampled[i] + alpha[3, h] * y_sampled[i]) for h in 1:3]) / sum([mu[h] * exp(alpha[1, h] + alpha[2, h] * x_sampled[i] + alpha[3, h] * y_sampled[i]) for h in 1:3]) for i in 1:n];
+    #@show norm(fw .- fw_tmp);
+
+    sum(@. logpdf(Normal(0, sigma), @. y_sampled - theta0 - x_sampled * theta1) +
+    log(fw) - log(denom) + log(PI))) : -Inf
+end
+
+logπ5 = function(eta; x_sampled = x_sampled::Vector{Float64},
+    y_sampled = y_sampled::Vector{Float64}, w_sampled = w_sampled::Vector{Float64},
+    nodes = nodes::Vector{Float64}, weights = weights::Vector{Float64})
+    #@show eta
+    theta0 = eta[1]
+    theta1 = eta[2]
+    sigma2 = eta[3]
+    mu = [eta[4], eta[4], eta[4]]
+    alpha = [zeros(3) eta[5:7] eta[8:10]]
+    #alpha = [[0.0 eta[7] eta[10]]; [0.0 eta[8] eta[11]]; [0.0 eta[9] eta[12]]]
+    phi = [eta[11], eta[11], eta[11]]
+
+    n = size(x_sampled)[1]
+    K = size(nodes)[1]
+
+    #return ((sigma2 > 0.0) & all(phi .> 0.0) & all(0.0 .< mu .< 1.0)) ? (
+    return ((sigma2 > 0.0) & all(phi .> 0.0) & all(0.0 < eta[4]  < 1.0)) ? (
+    sigma = sqrt(sigma2);
+    PI = (exp.(hcat(ones(n), x_sampled, y_sampled) * alpha) * ones(3))./
     (exp.(hcat(ones(n), x_sampled, y_sampled) * alpha) * ones(3));
 
     #PI_tmp = [sum([mu[h] * exp(alpha[1, h] + alpha[2, h] * x_sampled[i] + alpha[3, h] * y_sampled[i]) for h in 1:3]) / sum([exp(alpha[1, h] + alpha[2, h] * x_sampled[i] + alpha[3, h] * y_sampled[i]) for h in 1:3]) for i in 1:n];
     #@show all(PI .== PI_tmp);
 
-    denom = [sum((exp.(hcat(ones(K), fill(x_sampled[i], K), (@. nodes * sigma + theta0 + theta1 * x_sampled[i])) * alpha) * mu) ./
-    (exp.(hcat(ones(K), fill(x_sampled[i], K), (@. nodes * sigma + theta0 + theta1 * x_sampled[i])) * alpha) * ones(3)) .* weights)
+    denom = [sum((exp.(hcat(ones(K), fill(x_sampled[i], K),
+    (@. nodes * sigma + theta0 + theta1 * x_sampled[i])) * alpha) * ones(3)) ./
+    (exp.(hcat(ones(K), fill(x_sampled[i], K),
+    (@. nodes * sigma + theta0 + theta1 * x_sampled[i])) * alpha) * ones(3)) .* weights)
     for i in 1:n];
 
-    fw = [StatsBase.mean((@. pdf(BetaPrime((1 - mu) * phi, mu * phi + 1), w_sampled[i] - 1.0)), StatsBase.weights(vcat(exp.(hcat(1.0, x_sampled[i], y_sampled[i]) * alpha)...) .* mu))
+    fw = [StatsBase.mean((@. pdf(BetaPrime((1 - eta[4]) * eta[11], eta[4] * eta[11] + 1), w_sampled[i] - 1.0)))
     for i in 1:n];
     #fw_tmp = [sum([pdf(BetaPrime((1 - mu[h]) * phi[h], mu[h] * phi[h] + 1), w_sampled[i] - 1.0) * mu[h] * exp(alpha[1, h] + alpha[2, h] * x_sampled[i] + alpha[3, h] * y_sampled[i]) for h in 1:3]) / sum([mu[h] * exp(alpha[1, h] + alpha[2, h] * x_sampled[i] + alpha[3, h] * y_sampled[i]) for h in 1:3]) for i in 1:n];
     #@show norm(fw .- fw_tmp);
--- Bayesian/Canadian/main.jl
+++ Bayesian/Canadian/main.jl
@@ -29,13 +29,12 @@ WEIGHTS = WEIGHTS ./ sqrtπ
 x_sampled = log.(data[:, 3])
 x_sampled = x_sampled .- mean(x_sampled)
 y_sampled = log.(data[:, 4] .* 1000)
-w_sampled = data[:, 2]
+w_sampled = float.(data[:, 2])
 
 ## OLS Estimation
 ols = lm(@formula(y_sampled ~ x_sampled), DataFrame(x_sampled = x_sampled, y_sampled = y_sampled))
 theta_ini = coef(ols)
 sigma2_ini = deviance(ols) / dof_residual(ols)
-
 ## Maximum Likelihood Estimation
 logπ4(vcat(theta_ini, sigma2_ini, [1.0/2200.0, 1.0/750.0, 1.0/35.0], collect(1:6), collect(1:3)); x_sampled = x_sampled, y_sampled = y_sampled, w_sampled = w_sampled,
 nodes = NODES, weights = WEIGHTS)
@@ -45,11 +44,42 @@ nodes = NODES, weights = WEIGHTS)
 func = TwiceDifferentiable(eta -> -logπ4(eta; x_sampled = x_sampled, y_sampled = y_sampled, w_sampled = w_sampled,
 nodes = NODES, weights = WEIGHTS), vcat(theta_ini, sigma2_ini, fill(0.5, 3), ones(6), ones(3)); autodiff=:forward)
 
-optim = optimize(func, vcat(theta_ini, sigma2_ini, [1.0/2200.0, 1.0/750.0, 1.0/35.0], ones(6), ones(3)), NelderMead(), Optim.Options(iterations = 100000))
+optim = optimize(func, vcat(theta_ini, sigma2_ini, [1.0/2200.0, 1.0/750.0, 1.0/35.0], zeros(6), ones(3)), NelderMead(), Optim.Options(iterations = 100000))
 numerical_hessian = NLSolversBase.hessian!(func,optim.minimizer)
 @show optim
-@show optim.minimizer
+println("theta: ", optim.minimizer[1:2])
+println("sigma2: ", optim.minimizer[3])
+println("mu: ", optim.minimizer[4:6])
+println("alpha: ", optim.minimizer[7:12])
+println("phi: ", optim.minimizer[13:15])
 @show optim.minimum
+
+logπ4(optim.minimizer; x_sampled = x_sampled, y_sampled = y_sampled, w_sampled = w_sampled,
+nodes = NODES, weights = WEIGHTS)
+
+@. pdf(BetaPrime([1, 1], [3, 1]), 1.0)
+
+logπ5(vcat(theta_ini, sigma2_ini, [1.0/2200.0, 1.0/750.0, 1.0/35.0], zeros(6), 1); x_sampled = x_sampled, y_sampled = y_sampled, w_sampled = w_sampled,
+nodes = NODES, weights = WEIGHTS)
+#logπ4(vcat(zeros(2), 1.0, fill(0.5, 3), ones(6), ones(3)); x_sampled = x_sampled, y_sampled = y_sampled, w_sampled = w_sampled,
+#nodes = NODES, weights = WEIGHTS)
+
+func = TwiceDifferentiable(eta -> -logπ5(eta; x_sampled = x_sampled, y_sampled = y_sampled, w_sampled = w_sampled,
+nodes = NODES, weights = WEIGHTS), vcat(theta_ini, sigma2_ini, fill(0.5), zeros(6), 1); autodiff=:forward)
+
+optim = optimize(func, vcat(theta_ini, sigma2_ini, [1.0/2200.0], zeros(6), 100), NelderMead(), Optim.Options(iterations = 100000))
+numerical_hessian = NLSolversBase.hessian!(func,optim.minimizer)
+@show optim
+println("theta: ", optim.minimizer[1:2])
+println("sigma2: ", optim.minimizer[3])
+println("mu: ", optim.minimizer[4])
+println("alpha: ", optim.minimizer[5:10])
+println("phi: ", optim.minimizer[11])
+@show optim.minimum
+
+
+logπ4(vcat(optim.minimizer[1:3], [1.0/2200.0, 1.0/750.0, 1.0/35.0], optim.minimizer[7:15]); x_sampled = x_sampled, y_sampled = y_sampled, w_sampled = w_sampled,
+nodes = NODES, weights = WEIGHTS)
 
 optim = optimize(func, vcat(theta_ini, sigma2_ini, fill(0.5, 3), ones(6), ones(3)), Newton(), Optim.Options(iterations = 100000, g_tol = 1e-12))
 numerical_hessian = NLSolversBase.hessian!(func,optim.minimizer)
@@ -64,6 +94,34 @@ nodes = NODES, weights = WEIGHTS), vcat(theta_ini.-0.01, sigma2_ini-0.1, fill(0.
 @show optim.minimum
 
 @show res2 = sqrt.(diag(inv(numerical_hessian)))[4:6]
+
+#=
+tmpftn = (eta -> (0 < eta[1] < 1) & (eta[2] > 0) ? -sum(logpdf.(BetaPrime.((1 - eta[1]) * eta[2], eta[1] * eta[2] + 1), w_sampled[w_sampled .< 500] .- 1.0)) : Inf)
+w_sampled[w_sampled .< 500]
+tmpftn = (theta -> (0 < theta[1]) & (theta[2] > 0) ? -sum(logpdf.(BetaPrime.(theta[1], theta[2]), w_sampled[w_sampled .< 500] .- 1.0)) : Inf)
+tmpftn([0.1, 1])
+tmpftn([-0.1, 1])
+optim = optimize(tmpftn, [0.5, 1], NelderMead(), Optim.Options(iterations = 100000))
+optim.minimizer
+optim.minimizer[1] / (optim.minimizer[2]-1)
+optim.minimum
+
+tmpftn = function(eta)
+    mu = eta[1:3]
+    phi = eta[4:6]
+    p = vcat(eta[7:8], 1 - eta[7] - eta[8])
+    n = size(w_sampled)[1]
+    return all(0 .< mu .< 1) & all(phi .> 0) & all(0 .< p .< 1) ? (
+    vec = [sum([pdf(BetaPrime((1 - mu[h]) * phi[h], mu[h] * phi[h]), w_sampled[i]) * p[h] for h in 1:3]) for i in 1:n];
+    -sum(log.(vec))
+    ) : Inf
+end
+tmpftn(vcat(fill(0.5, 3), ones(3), fill(0.3, 2)))
+optim = optimize(tmpftn, vcat(fill(0.5, 3), ones(3), fill(0.3, 2)), NelderMead(), Optim.Options(iterations = 100000))
+optim
+optim.minimizer
+tmpftn = (eta -> (0 < eta[1] < 1) & (eta[2] > 0) ? -sum(logpdf.(BetaPrime.((1 - eta[1]) * eta[2], eta[1] * eta[2] + 1), w_sampled .- 1.0)) : Inf) 
+=#
 
 ## Design-based Estimation
 
